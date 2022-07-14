0c28b0c916f91943af7764daca01ecbc2394a25e
luizramirez <luis@onecloudsys.com>
2022-07-14 11:18:31 -0700

    bad code

--- handler.js
+++ handler.js
@@ -12,7 +12,7 @@ module.exports.hello = async (event) => {
       null,
       2
     ),
-  };
+  }
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
